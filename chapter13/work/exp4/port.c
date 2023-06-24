f0ce6f69eb311677ce1faef18754322afe2ceb22
hanyi <hanyiwsl@ywj.com>
2023-06-24 18:28:59 +0800

    view

---
+++ chapter13/work/exp4/port.c
@@ -0,0 +1,34 @@
+#include "port.h"
+
+Port::Port(const char *br = "none", const char * st = "none", int b=0)
+{
+    
+}
+
+Port::Port(const Port &p)
+{
+
+}
+
+Port & Port::operator=(const Port &p)
+{
+    
+}
+Port & Port::operator+=(int b)
+{
+
+}
+
+Port & Port::operator-=(int b)
+{
+
+}
+void Port::Show() const
+{
+    
+}
+
+friend ostream & Port::operator<<(ostream &os, const Port &p)
+{
+
+}
