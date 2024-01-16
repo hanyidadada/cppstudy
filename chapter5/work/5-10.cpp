c65158232013b4352fc3875e3814d67983bdfcbb
hanyi <hanyiwsl@ywj.com>
2024-01-16 23:28:02 +0800

    fix

--- chapter5/work/5-10.cpp
+++ chapter5/work/5-10.cpp
@@ -11,7 +11,7 @@ int main(){
         {
             std::cout << ".";
         }
-        for (int j = 0; j <= i; j++)
+        for (int j = 0; j < i; j++)
         {
             std::cout << "*";
         }
